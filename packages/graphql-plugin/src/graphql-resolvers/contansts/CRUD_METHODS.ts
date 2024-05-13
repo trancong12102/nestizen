cf0abfa410b85e76a8fbc82f10590e3acd68b2ca
Trần Công <trancong12102@gmail.com>
2024-05-13 13:07:11 +0700

    chore: update generate warning comment

--- packages/graphql-plugin/src/graphql-resolvers/contansts/CRUD_METHODS.ts
+++ packages/graphql-plugin/src/graphql-resolvers/contansts/CRUD_METHODS.ts
@@ -2,12 +2,8 @@ export const CRUD_METHODS = [
   'findUnique',
   'findMany',
   'create',
-  // 'createMany',
   'update',
-  // 'updateMany',
   'delete',
-  // 'deleteMany',
   'count',
   'aggregate',
-  // 'groupBy',
 ];
